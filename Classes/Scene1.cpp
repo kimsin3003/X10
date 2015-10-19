f9dd2cc1624acfc4dbc07b735e5dccbba5bc7f51
Paul-Kim <kmc5500@naver.com>
2015-10-19 13:05:17 +0900

    내 버튼 가운데로

--- Classes/Scene1.cpp
+++ Classes/Scene1.cpp
@@ -41,8 +41,8 @@ bool Scene1::init()
                                            "CloseSelected.png",
                                            CC_CALLBACK_1(Scene1::nextSceneCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width /2 - closeItem->getContentSize().width/2 ,
+                                origin.y + visibleSize.height /2 + closeItem->getContentSize().height/2));
 
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);

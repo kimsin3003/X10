6c5b558322cdd92b4f8685851dad2d36e6118ac6
FeatherHub <woook95@naver.com>
2015-10-19 11:39:52 +0900

    Return past

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -8,7 +8,10 @@ static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
+<<<<<<< HEAD
+AppDelegate::AppDelegate() {
 
+=======
 /*
 
 	작성자: 최진욱

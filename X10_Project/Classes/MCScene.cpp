49b32d84ce12eaf3130208e2ebf33775a02e38c9
Paul-Kim <kmc5500@naver.com>
2015-10-28 21:32:18 +0900

    add to MCscene all mission

--- X10_Project/Classes/MCScene.cpp
+++ X10_Project/Classes/MCScene.cpp
@@ -28,11 +28,30 @@ bool MCScene::init()
 	GotoMainSceneMenu->setPosition(200, 300);
 	this->addChild(GotoMainSceneMenu);
 
-	auto taewooMission1 = Label::createWithTTF("MC's test Page", "fonts/arial.ttf", 24);
+	auto taewooMission1 = Label::createWithTTF("JUMP!!!", "fonts/arial.ttf", 50);
 	taewooMission1->setPosition(Point(200, 150));
 	taewooMission1->setName("twMissionLabel");
 	taewooMission1->setVisible(false);
 	this->addChild(taewooMission1);
+
+	auto dir = Director::getInstance();
+	auto screen = dir->getVisibleSize();
+
+	auto jinwookMission = Sprite::create("boy 31x40.png");
+	auto boysize = jinwookMission->getContentSize();
+	jinwookMission->setPosition(0 + boysize.width / 2, 0 + boysize.height / 2);
+	jinwookMission->setName("boy");
+	this->addChild(jinwookMission);
+
+	auto moveToRL = MoveBy::create(1, Point(screen.width - boysize.width, 0));
+	auto moveToRU = MoveBy::create(1, Point(0 , screen.height - boysize.height));
+	auto moveToLU = MoveBy::create(1, Point( -screen.width + boysize.width, 0));
+	auto moveToLL = MoveBy::create(1, Point(0, -screen.height + boysize.height));
+
+	auto moveArround = Sequence::create(moveToRL, moveToRU, moveToLU, moveToLL, NULL);
+	auto repeat_action = RepeatForever::create(moveArround);
+
+	jinwookMission->runAction(repeat_action);
 
 	auto _mouseListener = EventListenerMouse::create();
 	_mouseListener->onMouseUp = CC_CALLBACK_1(MCScene::onMouseUp, this);
@@ -49,12 +68,16 @@ void MCScene::ChangeToMainScene(Ref* pSender)
 
 void MCScene::onMouseUp(Event *event)
 {
-	auto target = this->getChildByName("twMissionLabel");
-	target->setVisible(false);
+	auto label = this->getChildByName("twMissionLabel");
+	label->setVisible(false);
 }
 
 void MCScene::onMouseDown(Event *event)
 {
-	auto target = this->getChildByName("twMissionLabel");
-	target->setVisible(true);
+	auto e = (EventMouse*)event;
+	auto blinkLabel = this->getChildByName("twMissionLabel");
+	blinkLabel->setVisible(true);
+	auto runningBoy = this->getChildByName("boy");
+	auto moveToMouse = JumpBy::create(0.3, Point(0, 0), 50, 1);
+	runningBoy->runAction(moveToMouse);
 }

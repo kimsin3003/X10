b4c8a2f036f86558221dbb9d12615156d26b4247
Taewoo Kim <kimsin3003@naver.com>
2015-10-30 13:48:29 +0900

    backgroud resource

--- X10_Project/Classes/TWScene.cpp
+++ X10_Project/Classes/TWScene.cpp
@@ -15,7 +15,7 @@ Scene* TWScene::createScene()
 
 bool TWScene::init()
 {
-	if (!Layer::init())
+	if (!CCLayerColor::initWithColor(Color4B::BLUE))
 	{
 		return false;
 	}
@@ -37,7 +37,7 @@ bool TWScene::init()
 	auto forever_repeat_action = RepeatForever::create(sequence_action);
 
 	mainTitle->runAction(forever_repeat_action);
-	mainTitle->stopAction(forever_repeat_action);
+	//mainTitle->stopAction(forever_repeat_action);
 	this->addChild(mainTitle);
 
 	auto listener = EventListenerMouse::create();
@@ -46,9 +46,13 @@ bool TWScene::init()
 
 
 	//2회차 스터디
+	Sprite* character = Sprite::create();
+	this->addChild(character);
+	character->setPosition(Point(winSize.width / 2, winSize.height / 2));
 	Vector<SpriteFrame*> animFrames;
-	animFrames.reserve(12);
-	animFrames.pushBack(SpriteFrame::create("Blue_Front1.png", Rect(0, 0, 65, 81)));
+	animFrames.reserve(1);
+	for (int i = 0; i < 4; i++)
+		animFrames.pushBack(SpriteFrame::create("res/lisa.png", Rect(0, 48 * i, 27, 48)));
 
 	// create the animation out of the frame
 	Animation* animation = Animation::createWithSpriteFrames(animFrames, 0.1f);
@@ -57,9 +61,58 @@ bool TWScene::init()
 	// run it and repeat it forever
 	RepeatForever *action = RepeatForever::create(animate);
 	
-	return true;
+	character->runAction(action);
+
+	this->schedule(schedule_selector(TWScene::ChangeBackground), 1.0);`
+
+	auto bgLayer = Layer::create();
+	this->addchild(bgLayer);
+
+	auto spr_0 = Sprite::create("bg1.png");
+	spr_0->setAnchorPoint(Point::ZERO);
+	spr_0->setPosition(Point::ZERO);
+	bgLayer->addChild(spr_0);
+
+	auto action_0 = MoveBy::create(10.0, Rect(-2000, 0));
+	auto action_1 = Place::create(Point::ZERO);
+	auto action_2 = Sequence::create(action_0, action_1, NULL);
+	auto action_3 = RepeatForever::create(2);
+	bgLayer->runAction(action_3);
 
 	return true;
+}
+
+void TWScene::ChangeBackground(float deltaTIme)
+{
+	Color3B color;
+	int rand = random() % 5;
+	switch (rand){
+	case 0:  
+		color = Color3B::YELLOW;
+		break;
+	case 1:
+		color = Color3B::BLUE;
+		break;
+	case 2:
+		color = Color3B::GREEN;
+		break;
+	case 3:
+		color = Color3B::RED;
+		break;
+	case 4:
+		color = Color3B::MAGENTA;
+		break;
+	case 5:
+		color = Color3B::BLACK;
+		break;
+	case 6:
+		color = Color3B::ORANGE;
+		break;
+	case 7:
+		color = Color3B::GRAY;
+		break;
+	}
+	this->setColor(color);
 }
 
 void TWScene::ChangeToMainScene(Ref* pSender)

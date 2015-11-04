1b4ce442430dda1ad1aab1423cb9527a361f20f7
FeatherHub <woook95@naver.com>
2015-11-04 22:43:09 +0900

    Empty Scene

--- X10_Project/Classes/GameManager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class GameManager
-{
-public:
-	GameManager();
-	~GameManager();
-};
-
---
+++ X10_Project/Classes/MainScene.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "MainScene.h"
+
+Scene* MainScene::createScene()
+{
+    auto scene = Scene::create();
+    
+    auto layer = MainScene::create();
+
+    scene->addChild(layer);
+
+    return scene;
+}
+
+bool MainScene::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	auto closeItem = MenuItemImage::create(
+		"CloseNormal.png",
+		"CloseSelected.png",
+		CC_CALLBACK_1(MainScene::menuCloseCallback, this));
+
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
+		origin.y + closeItem->getContentSize().height / 2));
+
+	auto menu = Menu::create(closeItem, NULL);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 1);
+}
+
+void MainScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}

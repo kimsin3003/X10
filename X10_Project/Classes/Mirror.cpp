8ebf0c31590f610733ea04f5c39b55970788a671
Paul-Kim <kmc5500@naver.com>
2015-12-18 12:06:51 +0900

    change Mirror's reflextion algorithm

--- X10_Project/Classes/Mirror.cpp
+++ X10_Project/Classes/Mirror.cpp
@@ -12,15 +12,25 @@ bool Mirror::init()
 	}
 	m_spr = Sprite::create(FileStuff::MIRROR);
 	addChild(m_spr);
-	Size sizeSpr = m_spr->getContentSize();
-	Size sizeMain = this->getContentSize();
 	return true;
 }
 
 void Mirror::ToBullet(Bullet* bullet)
 {
+	float mirrorAngle = getRotation(); //거울이 수평선에서 기울어진 정도 (시계방향)
+	Vec2 bulletDir = bullet->GetDirection(); // 불렛의 속도벡터
+	
+	float mirrorRadian = CC_DEGREES_TO_RADIANS(mirrorAngle);
+	bulletDir.rotate(Vec2::ZERO, mirrorRadian); // 벡터를 수평 죄표계로 회전 (거울이 수직일 경우로 계산하기 위해...)
+	
+	bulletDir.x = -bulletDir.x; //가상의 수직 거울에 반사
+	bulletDir.rotate(Vec2::ZERO, -mirrorRadian); // 원래 좌표계뢰 다시 회전
+	
+	bullet->SetDirection(bulletDir);
+	getBoundingBox();
+
 	//미러 기준의 입사각.
-	float m = this->getRotation();
+	/*float m = this->getRotation();
 	float inci_angle = (-bullet->GetDirection()).getAngle() - this->getRotation();
 	if (inci_angle <= 180){
 		float ref_angle = (180 - inci_angle) + this->getRotation();
@@ -30,7 +40,7 @@ void Mirror::ToBullet(Bullet* bullet)
 	else{
 		float ref_angle = (540 - inci_angle) + this->getRotation();
 		bullet->SetDirection(Vec2(cos(ref_angle), sin(ref_angle)));
-	}
+	}*/
 }
 
 void Mirror::ToSelf(const Bullet* bullet)

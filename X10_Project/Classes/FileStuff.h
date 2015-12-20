10d157a04b322306a3701055cb442eda5d7fc248
Paul-Kim <kmc5500@naver.com>
2015-12-20 17:08:52 +0900

    merge complete

--- X10_Project/Classes/FileStuff.h
+++ X10_Project/Classes/FileStuff.h
@@ -4,7 +4,7 @@ namespace FileStuff
 {
 	const string IMG_SOURCE = "res/target/x10.plist";
 
-	const string BACKGROUND = "res/background_base.png";
+	const string BACKGROUND_BASE = "res/background_base.png";
 	const string BACKGROUND_BOTTOM = "res/background_bottom.png";
 
 	const string CHARACTER = "black_man.png";
@@ -44,16 +44,8 @@ namespace FileStuff
 	const string BOTTLE = "res/collection/bottle.png";
 	const string MONITOR = "res/collection/monitor.png";
 
-<<<<<<< HEAD
-	const string CHARACTER = "res/character/boy.png";
-	const string CHARACTER_HARDPIXEL = "res/character/boy_hardpixel.png";
-	const string CHARACTER_STANDING = "res/character/boy_standing.png";
-	const string CHARACTER_ARM = "res/character/boy_arm.png";
-	const string SLING_LINE_DOT = "res/character/dot.png";
-
 	const string BACKGROUND = "res/highway.png";
-=======
->>>>>>> ada70e77929da683e649e3b34cc3a07323241406
+
 	const string START_IMG = "res/start.png";
 	const string STAGE_SELECT_IMG = "res/stage_select.png";
 	const string CLOSE_BUTTON = "closeButton.png";

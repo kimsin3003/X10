886ea8e37b2f0b4d57913a70a3f1b5ae330b563a
Paul-Kim <kmc5500@naver.com>
2016-02-23 15:25:48 +0900

    add adMob code

---
+++ X10_Project/proj.win32/AdmobHelper.h
@@ -0,0 +1,14 @@
+
+#ifndef  __ADMOB_HELPER_H_
+#define  __ADMOB_HELPER_H_
+
+class AdmobHelper
+{
+public:
+	static void hideAd();
+	static void showAd();
+	static bool isAdShowing;
+};
+
+
+#endif // __ADMOB_HELPER_H_

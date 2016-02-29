ebcd4a4a8ad2fe17b3ee4b123e944d9c53921710
Paul-Kim <kmc5500@naver.com>
2016-02-29 15:42:23 +0900

    delete remnant code in Appactivity java

--- X10_Project/proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ X10_Project/proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -27,23 +27,9 @@ THE SOFTWARE.
 
 package org.cocos2dx.cpp;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.cocos2dx.lib.Cocos2dxActivity;
 
-import android.R;
-import android.annotation.TargetApi;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.LinearLayout;
-
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -102,109 +88,4 @@ public class AppActivity extends Cocos2dxActivity
 		
 	}
 	
-	
-//	private static AppActivity _appActivity;
-//	private InterstitialAd mInterstitialAd;
-//	private static final String AD_UNIT_ID = "ca-app-pub-1690383002847701/2009330075";
-//
-//	
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState)
-//	{
-//		super.onCreate(savedInstanceState);
-//
-//		_appActivity.runOnUiThread(new Runnable() {	
-//			@Override
-//			public void run() {
-//				// TODO Auto-generated method stub
-//
-//				mInterstitialAd.setAdUnitId(AD_UNIT_ID);
-//				mInterstitialAd.setAdListener(new AdListener(){
-//					@Override
-//					public void onAdClosed() {
-//						requestNewInterstitial();
-//					}
-//				});
-//				
-//						
-//			}
-//		});
-//		
-//		requestNewInterstitial();
-//
-//		_appActivity = this;
-//		
-//	}
-//	
-//	
-//	public static void hideAd()    
-//	{
-//	     _appActivity.runOnUiThread(new Runnable()
-//	     {
-//		     @Override
-//		     public void run()
-//		     {
-//		    	 
-//		     }
-//	     });
-//	}
-//	
-//	
-//	public static void showAd()
-//	{
-//		_appActivity.runOnUiThread(new Runnable()
-//		{
-//			 @Override
-//			 public void run()
-//			 {	
-//				if(_appActivity.mInterstitialAd.isLoaded())
-//					_appActivity.mInterstitialAd.show();	
-//			 }
-//	     });
-//	}
-//	
-//	private void requestNewInterstitial() {
-//		_appActivity.runOnUiThread(new Runnable() {
-//			@Override
-//			public void run() {
-//				// TODO Auto-generated method stub
-//
-//		        AdRequest adRequest = new AdRequest.Builder()
-//		        		.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-//		        		.addTestDevice("717495186B4955BDA7D6A6B644CB1D72") //MC's Luna phone
-//		        		.addTestDevice("8F7E2107BC04D9292482870C9BC1CDB0") //JW's G2 phone
-//		        		.build();
-//
-//		        mInterstitialAd.loadAd(adRequest);
-//			}
-//		});			
-//	}
-//	
-//
-//	@Override
-//	protected void onResume() 
-//	{
-//		super.onResume();
-//		if (mInterstitialAd != null) 
-//		{
-//
-//		}
-//	}
-//	
-//	@Override
-//	protected void onPause() 
-//	{
-//		if (mInterstitialAd != null) 
-//		{
-//			
-//		}
-//		super.onPause();
-//	}
-//	
-//    @Override
-//    protected void onDestroy() 
-//    {
-//        super.onDestroy();
-//    }
-//	
 }
